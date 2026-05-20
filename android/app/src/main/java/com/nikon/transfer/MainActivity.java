a9ae22bd0e78f19a77e029eb403a2894cd60800f
wangjinjing <2723217360@qq.com>
2026-05-20 11:19:28 +0800

    Avoid drawing under system navigation bar

--- android/app/src/main/java/com/nikon/transfer/MainActivity.java
+++ android/app/src/main/java/com/nikon/transfer/MainActivity.java
@@ -2,10 +2,12 @@ package com.nikon.transfer;
 
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
         registerPlugin(NikonCameraPlugin.class);
         super.onCreate(savedInstanceState);
     }
